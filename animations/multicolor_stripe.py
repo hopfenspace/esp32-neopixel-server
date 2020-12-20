06e85bc7d44990eb4dfd5a7ad350c4fa1fcb8cae
Jakob Löw <jakob@m4gnus.de>
2020-12-20 23:19:01 +0100

    animation multicolor_stripe: fix indentation

--- animations/multicolor_stripe.py
+++ animations/multicolor_stripe.py
@@ -6,39 +6,39 @@ NUM_PIXEL = 300
 
 colors = [
 	0x220000,
-    0x000000,
-    0x000000,
-    0x000000,
+	0x000000,
+	0x000000,
+	0x000000,
 	0x002200,
-    0x000000,
-    0x000000,
-    0x000000,
+	0x000000,
+	0x000000,
+	0x000000,
 	0x222200,
-    0x000000,
-    0x000000,
-    0x000000,
+	0x000000,
+	0x000000,
+	0x000000,
 	0x220022,
-    0x000000,
-    0x000000,
-    0x000000,
+	0x000000,
+	0x000000,
+	0x000000,
 	0x002222,
-    0x000000,
-    0x000000,
-    0x000000,
+	0x000000,
+	0x000000,
+	0x000000,
 	0x000022,
-    0x000000,
-    0x000000,
-    0x000000,
+	0x000000,
+	0x000000,
+	0x000000,
 ]
 
 colors = (NUM_PIXEL // len(colors)) * colors
 stripe = ESP32NeopixelServer(HOST)
 
 while True:
-    stripe.setMultiplePixel(0, colors)
-    stripe.show()
+	stripe.setMultiplePixel(0, colors)
+	stripe.show()
 
-    first = colors[0]
-    colors = colors[1 : ] + [first]
+	first = colors[0]
+	colors = colors[1 : ] + [first]
 
-    time.sleep(0.5)
+	time.sleep(0.5)
